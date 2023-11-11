2c985be3c9909f68e23436e815a992262eb4ab50
Mitch Aureli <themitchellandrews@gmail.com>
2023-11-10 20:41:53 -0500

    Fixed Vite Rollup

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
             input: {
                 main: path.resolve(__dirname, 'index.html'),
                 bibleangel: path.resolve(__dirname, 'projects/bibleangel/index.html'),
-                cubeofagression: path.resolve(__dirname, 'projects/cubeofagression/index.html'),
+                cubeofaggression: path.resolve(__dirname, 'projects/cubeofaggression/index.html'),
+                mirrortest: path.resolve(__dirname, 'projects/mirrortest/index.html'),
+                tunnelvision: path.resolve(__dirname, 'projects/tunnelvision/index.html'),
                 // add more projects as needed
             },
         },
